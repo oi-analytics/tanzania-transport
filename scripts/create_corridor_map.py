cd7d0a14a28bc7495888497af63ce0867edb624a
Tom Russell <tomalrussell@gmail.com>
2017-07-28 18:37:43 +0100

    Plot elements of corridor map

--- scripts/create_corridor_map.py
+++ scripts/create_corridor_map.py
@@ -1,15 +1,18 @@
 """Generate transport corridor map, showing major routes and nodes for each mode
 """
 # pylint: disable=C0103
+import csv
 import os
 
 import cartopy.crs as ccrs
 import cartopy.io.shapereader as shpreader
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
 # Input data
 base_path = os.path.join(os.path.dirname(__file__), '..')
 data_path = os.path.join(base_path, 'data')
+resource_path = os.path.join(base_path, 'resources')
 
 # TZ_TransNet_Roads, clipped to Tanzania
 road_filename = os.path.join(data_path, 'Road_data', 'TZ_TransNet_Roads.shp')
@@ -19,18 +22,142 @@ railway_filename = os.path.join(data_path, 'Railway_data', 'TZ_TransNet_Railroad
 
 # TZ_TransNet_FerryTerminals and TZ_TransNet_FerryRoutes
 ferry_path = os.path.join(data_path, 'Port_data')
-ferry_terminals = os.path.join(ferry_path, 'TZ_TransNet_FerryTerminals.shp')
-ferry_routes = os.path.join(ferry_path, 'TZ_TransNet_FerryRoutes.shp')
+ferry_terminals_filename = os.path.join(ferry_path, 'TZ_TransNet_FerryTerminals.shp')
+ferry_routes_filename = os.path.join(ferry_path, 'TZ_TransNet_FerryRoutes.shp')
 
 # tanzania_airports from ourairports.com
-airport_path = os.path.join(data_path, 'Airport_data', 'tanzania_airports.csv')
+airport_filename = os.path.join(data_path, 'Airport_data', 'tanzania_airports.csv')
 
 # Natural Earth countries
 states_filename = os.path.join(data_path, 'Boundary_datasets',
                                'ne_10m_admin_0_countries_lakes.shp')
 
-# Read in Tanzania outline
+# Icons
+boat_icon_filename = os.path.join(resource_path, 'boat.png')
+plane_icon_filename = os.path.join(resource_path, 'plane.png')
+train_icon_filename = os.path.join(resource_path, 'train.png')
+
+# Create figure
+plt.figure(figsize=(10, 10), dpi=150)
+
+proj_lat_lon = ccrs.PlateCarree()
+proj_3857 = ccrs.epsg(3857)
+ax = plt.axes([0.025, 0.025, 0.95, 0.93], projection=proj_lat_lon)
+x0 = 28.6
+x1 = 41.4
+y0 = 0.5
+y1 = -12.5
+ax.set_extent([x0, x1, y0, y1], crs=proj_lat_lon)
+
+# Africa, for Tanzania and neighbours
 for record in shpreader.Reader(states_filename).records():
-    country_code = record.attributes["ISO_A2"]
-    if country_code == "TZ":
-        tz_geom = record.geometry
+    if record.attributes['CONTINENT'] != 'Africa':
+        continue
+
+    geom = record.geometry
+    ax.add_geometries(
+        [geom],
+        crs=proj_lat_lon,
+        edgecolor='white',
+        facecolor='#efefef',
+        zorder=1)
+
+# Major roads
+for record in shpreader.Reader(road_filename).records():
+    geom = record.geometry
+    country = record.attributes["Country"]
+    if country == "Tanzania":
+        ax.add_geometries(
+            [geom],
+            crs=proj_3857,
+            edgecolor='#1f78b4',
+            facecolor='none',
+            zorder=2)
+
+# Railways
+for record in shpreader.Reader(railway_filename).records():
+    geom = record.geometry
+    country = record.attributes["Country"]
+    if country == "Tanzania":
+        ax.add_geometries(
+            [geom],
+            crs=proj_3857,
+            edgecolor='#33a02c',
+            facecolor='none',
+            zorder=3)
+
+# Ferry routes
+for record in shpreader.Reader(ferry_routes_filename).records():
+    geom = record.geometry
+    ax.add_geometries(
+        [geom],
+        crs=proj_3857,
+        edgecolor='#ff7f00',
+        facecolor='none',
+        zorder=3)
+
+# Ferry ports
+ferry_im = plt.imread(boat_icon_filename)
+for record in shpreader.Reader(ferry_terminals_filename).records():
+    geom = record.geometry
+    offset = 15000
+    img_extent = (
+        geom.x - offset,
+        geom.x + offset,
+        geom.y - offset,
+        geom.y + offset
+    )
+    ax.imshow(ferry_im, origin='upper', extent=img_extent, transform=proj_3857, zorder=4)
+
+# Airports
+plane_im = plt.imread(plane_icon_filename)
+with open(airport_filename, 'r') as airports_file:
+    reader = csv.DictReader(airports_file)
+    for line in reader:
+        x = float(line['longitude_deg'])
+        y = float(line['latitude_deg'])
+
+        # Small airports with smaller icon
+        if line['type'] == 'large_airport':
+            offset = 0.2
+        elif line['type'] == 'medium_airport':
+            offset = 0.125
+        else:
+            offset = 0.1
+
+        # Nudge airports which are next to ferry ports
+        if line['name'] == 'Bukoba Airport':
+            x -= 0.25
+        elif line['name'] == 'Musoma Airport':
+            x += 0.2
+        elif line['name'] == 'Mwanza Airport':
+            x += 0.2
+            y -= 0.1
+
+        img_extent = (
+            x - offset,
+            x + offset,
+            y - offset,
+            y + offset
+        )
+        ax.imshow(plane_im, origin='upper', extent=img_extent, transform=proj_lat_lon, zorder=5)
+
+# Legend
+legend_handles = [
+    mpatches.Patch(color='#1f78b4', label='Corridor Roads'),
+    mpatches.Patch(color='#33a02c', label='Railways'),
+    mpatches.Patch(color='#ff7f00', label='Ferry routes'),
+]
+plt.legend(
+    handles=legend_handles,
+    loc='lower left'
+)
+plt.title('Major Transport Corridors in Tanzania')
+
+
+output_filename = os.path.join(
+    base_path,
+    'figures',
+    'corridor_map.png'
+)
+plt.savefig(output_filename)
