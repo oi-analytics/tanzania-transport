5a8dbefb391bdd111a027a2f55b2ba107bcf8610
Tom Russell <tomalrussell@gmail.com>
2018-01-04 18:06:59 +0000

    Create road network map from spof file

--- scripts/create_road_network_map.py
+++ scripts/create_road_network_map.py
@@ -3,12 +3,12 @@
 # pylint: disable=C0103
 import os
 
-from utils import plot_pop, plot_countries, plot_regions
-
 import cartopy.crs as ccrs
 import cartopy.io.shapereader as shpreader
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+
+from utils import plot_pop, plot_countries, plot_regions
 
 # Input data
 base_path = os.path.join(os.path.dirname(__file__), '..')
@@ -18,8 +18,8 @@ states_filename = os.path.join(data_path, 'Infrastructure', 'Boundaries',
                                'ne_10m_admin_0_countries_lakes.shp')
 
 # Roads
-regional_road_filename = os.path.join(data_path, 'Infrastructure', 'Roads', 'Tanroads_flow_shapefiles', 'region_roads_2017.shp')
-trunk_road_filename = os.path.join(data_path, 'Infrastructure', 'Roads', 'Tanroads_flow_shapefiles', 'trunk_roads_2017.shp')
+all_road_filename = os.path.join(
+    data_path, 'Analysis_results', 'spof_localfailure_results', 'tz_road_spof_geom.shp')
 
 # Create figure
 plt.figure(figsize=(6, 6), dpi=150)
@@ -38,32 +38,23 @@ plot_countries(ax, data_path)
 plot_pop(plt, ax, data_path)
 plot_regions(ax, data_path)
 
-# Regional roads
-for record in shpreader.Reader(regional_road_filename).records():
-    geom = record.geometry
-    ax.add_geometries(
-        [geom],
-        crs=proj_lat_lon,
-        edgecolor='#ed9a36',
-        linewidth=3,
-        facecolor='none',
-        zorder=2)
+# All roads, including trunk
+all_road_geoms = [
+    record.geometry
+    for record in shpreader.Reader(all_road_filename).records()
+]
 
-# Trunk roads
-for record in shpreader.Reader(trunk_road_filename).records():
-    geom = record.geometry
-    ax.add_geometries(
-        [geom],
-        crs=proj_lat_lon,
-        linewidth=3,
-        edgecolor='#d1170a',
-        facecolor='none',
-        zorder=3)
+ax.add_geometries(
+    all_road_geoms,
+    crs=proj_lat_lon,
+    linewidth=1,
+    edgecolor='#d1170a',
+    facecolor='none',
+    zorder=2)
 
 # Legend
 legend_handles = [
-    mpatches.Patch(color='#d1170a', label='Trunk Roads'),
-    mpatches.Patch(color='#ed9a36', label='Regional Roads'),
+    mpatches.Patch(color='#d1170a', label='TANROADS Trunk and Regional Roads'),
 ]
 plt.legend(
     handles=legend_handles,
@@ -75,6 +66,6 @@ plt.title('Trunk and Regional Roads in Tanzania')
 output_filename = os.path.join(
     base_path,
     'figures',
-    'road_network_map.png'
+    'road_network_map_all.png'
 )
 plt.savefig(output_filename)
