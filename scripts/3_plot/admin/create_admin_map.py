22fba530852afbf70d826da16752d4eb503ef24e
Tom Russell <tomalrussell@gmail.com>
2018-04-03 10:49:47 +0100

    Update admin map

--- scripts/3_plot/admin/create_admin_map.py
+++ scripts/3_plot/admin/create_admin_map.py
@@ -1,6 +1,7 @@
 """Generate administrative map of Tanzania's regions and neighbouring countries
 """
 # pylint: disable=C0103
+import json
 import os
 
 import cartopy.crs as ccrs
@@ -8,21 +9,24 @@ import cartopy.io.shapereader as shpreader
 import matplotlib.pyplot as plt
 
 # Input data
-base_path = os.path.join(
-    os.path.dirname(__file__),
-    '..'
-)
+with open('config.json', 'r') as config_fh:
+    config = json.load(config_fh)
 
-data_path = os.path.join(
-    base_path,
-    'data'
-)
+data_path = config['data_path']
+figures_path = config['figures_path']
 
 states_filename = os.path.join(
     data_path,
     'Infrastructure',
     'Boundaries',
     'ne_10m_admin_0_countries_lakes.shp'
+)
+
+states_over_lakes_filename = os.path.join(
+    data_path,
+    'Infrastructure',
+    'Boundaries',
+    'ne_10m_admin_0_countries.shp'
 )
 
 provinces_filename = os.path.join(
@@ -32,7 +36,12 @@ provinces_filename = os.path.join(
     'ne_10m_admin_1_states_provinces_lakes.shp'
 )
 
-plt.figure(figsize=(6, 6), dpi=150)
+output_filename = os.path.join(
+    figures_path,
+    'admin_map.png'
+)
+
+plt.figure(figsize=(7, 7), dpi=300)
 
 proj = ccrs.PlateCarree()
 ax = plt.axes([0.025, 0.025, 0.95, 0.93], projection=proj)
@@ -47,7 +56,7 @@ for record in shpreader.Reader(states_filename).records():
     country_code = record.attributes['ISO_A2']
     if country_code in ('BI', 'RW', 'CD', 'UG', 'KE', 'ZM', 'MW', 'MZ', 'SO'):
         geom = record.geometry
-        ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#efefef')
+        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor='#e0e0e0')
 
 # Neighbour labels
 neighbours = [
@@ -103,27 +112,35 @@ for neighbour in neighbours:
         transform=proj)
 
 nudge_regions = {
-    'Dar-Es-Salaam': (-0.35, 0),
     'Kagera': (0.2, 0),
-    'Kaskazini-Pemba': (0, 0.2),
-    'Kaskazini-Unguja': (0, 0.1),
-    'Kusini-Pemba': (0, -0.2),
-    'Manyara': (0.1, 0.1),
+    'Kilimanjaro': (0.2, 0),
+    'Manyara': (0.15, 0.1),
+    'Morogoro': (0.25, 0),
     'Pwani': (0.1, -0.2),
-    'Zanzibar South and Central': (-0.3, -0.25),
 }
+
+no_label_regions = [
+    'Kaskazini-Pemba',
+    'Kaskazini-Unguja',
+    'Kusini-Pemba',
+    'Zanzibar South and Central',
+    'Zanzibar West'
+]
 
 # Regions
 for record in shpreader.Reader(provinces_filename).records():
     country_code = record.attributes['iso_a2']
     if country_code == 'TZ':
         geom = record.geometry
-        ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#d7d7d7')
+        ax.add_geometries([geom], crs=proj, edgecolor='#ffffff', facecolor='#d2d2d2')
 
         centroid = geom.centroid
         cx = geom.centroid.x
         cy = geom.centroid.y
         name = record.attributes['name']
+
+        if name in no_label_regions:
+            continue
 
         if name in nudge_regions:
             dx, dy = nudge_regions[name]
@@ -144,8 +161,15 @@ for record in shpreader.Reader(provinces_filename).records():
             horizontalalignment=ha,
             transform=proj)
 
-plt.title('Neighbouring Countries and Regions of Tanzania')
+# Tanzania, political border
+for record in shpreader.Reader(states_over_lakes_filename).records():
+    country_code = record.attributes['ISO_A2']
+    if country_code == 'TZ':
+        geom = record.geometry
+        ax.add_geometries([geom], crs=proj, edgecolor='#a0a0a0', facecolor='#00000000')
 
+
+# Africa, for neighbours map
 ax = plt.axes([0.0375, 0.025, 0.25, 0.25], projection=proj)
 x0 = 8
 x1 = 52
@@ -153,25 +177,14 @@ y0 = 10
 y1 = -37
 ax.set_extent([x0, x1, y0, y1], crs=proj)
 
-# Africa, for neighbours map
 for record in shpreader.Reader(states_filename).records():
     if record.attributes['CONTINENT'] == 'Africa':
         geom = record.geometry
         country_code = record.attributes['ISO_A2']
         if country_code == 'TZ':
-            ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#d7d7d7')
-            # plt.text(
-            #     geom.centroid.x,
-            #     geom.centroid.y,
-            #     'Tanzania',
-            #     horizontalalignment='left',
-            #     transform=proj)
+            ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#c4c4c4')
         else:
-            ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#efefef')
+            ax.add_geometries([geom], crs=proj, edgecolor='white', facecolor='#e0e0e0')
 
-output_filename = os.path.join(
-    base_path,
-    'figures',
-    'admin_map.png'
-)
 plt.savefig(output_filename)
+plt.savefig(output_filename.replace("png", "svg"))
