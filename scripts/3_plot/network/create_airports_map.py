3526388d7412b25e9529817508ff26d88e507fb1
Tom Russell <tomalrussell@gmail.com>
2018-04-04 12:52:05 +0100

    Update airports map

--- scripts/3_plot/network/create_airports_map.py
+++ scripts/3_plot/network/create_airports_map.py
@@ -1,92 +1,94 @@
 """Generate airport map
 """
 # pylint: disable=C0103
-import csv
 import os
-
-from utils import plot_pop, plot_countries, plot_regions
+import sys
 
 import cartopy.crs as ccrs
 import cartopy.io.shapereader as shpreader
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
-# Input data
-base_path = os.path.join(os.path.dirname(__file__), '..')
-data_path = os.path.join(base_path, 'data')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+from scripts.utils import *
+
+config = load_config()
+data_path = config['data_path']
+figures_path = config['figures_path']
 
 # tanzania_airports from ourairports.com
-airport_filename = os.path.join(data_path, 'Infrastructure', 'Airports', 'TZ_airport_node_flows.csv')
+airport_filename = os.path.join(data_path, 'Infrastructure', 'Airports',
+                                'airport_shapefiles', 'tz_od_airport_nodes.shp')
+output_filename = os.path.join(figures_path, 'airports_map.png')
 
 # Create figure
-plt.figure(figsize=(6, 6), dpi=150)
+ax = get_tz_axes()
 
 proj_lat_lon = ccrs.PlateCarree()
 proj_3857 = ccrs.epsg(3857)
-ax = plt.axes([0.025, 0.025, 0.95, 0.93], projection=proj_lat_lon)
-x0 = 28.6
-x1 = 41.4
-y0 = 0.5
-y1 = -12.5
-ax.set_extent([x0, x1, y0, y1], crs=proj_lat_lon)
 
 # Background
-plot_countries(ax, data_path)
-plot_pop(plt, ax, data_path)
-plot_regions(ax, data_path)
+plot_basemap(ax, data_path)
+plot_basemap_labels(ax, data_path)
+scale_bar(ax, length=100, location=(0.925,0.02))
 
 # Airports
+selected_color = '#5b1fb4'
+major_color = '#b41f78'
 xs = []
 ys = []
-with open(airport_filename, 'r') as airports_file:
-    reader = csv.DictReader(airports_file)
-    for line in reader:
-        x = float(line['longitude_deg'])
-        y = float(line['latitude_deg'])
-        xs.append(x)
-        ys.append(y)
-        name = line['name']
-        if name in [
-            'Julius Nyerere International Airport',
-            'Kilimanjaro International Airport',
-            'Abeid Amani Karume International Airport',
-            'Mbeya Airport',
-            'Pemba Airport',
-            ]:
-            y -= 0.35
-        else:
-            y += 0.05
+minor_xs = []
+minor_ys = []
+for record in shpreader.Reader(airport_filename).records():
+    if record.attributes['iso_countr'] != 'TZ':
+        continue
 
-        if name in [
-            'Abeid Amani Karume International Airport',
-            'Mtwara Airport',
-            'Julius Nyerere International Airport',
-            'Lake Manyara Airport',
-            'Mafia Island Airport',
-            'Pemba Airport',
-            'Tanga Airport',
-            'Songwe Airport',
-            'Dodoma Airport'
-            ]:
-            x -= 0.05
-            align = 'right'
-        else:
-            x += 0.05
-            align = 'left'
+    geom = record.geometry
+    x = geom.x
+    y = geom.y
 
-        name = name.replace(' Airport', '')
-        name = name.replace(' International', '')
-        ax.text(x, y, name, transform=proj_lat_lon, zorder=4, ha=align)
+    name = record.attributes['name']
+    name = name.replace(' Airport', '')
+    name = name.replace(' International', '')
 
-ax.scatter(xs, ys, facecolor='#b41f78', s=5, zorder=3)
+    if name not in (
+            'Julius Nyerere',
+            'Arusha',
+            'Kilimanjaro'):
 
+        minor_xs.append(x)
+        minor_ys.append(y)
+        continue
 
+    xs.append(x)
+    ys.append(y)
 
-plt.title('Major Airports in Tanzania')
+    if name == 'Arusha':
+        y += 0.07
+        x += 0.05
+        align = 'right'
+    elif name == 'Julius Nyerere':
+        y -= 0.1
+        x -= 0.05
+        align = 'right'
+    else:
+        y += 0.05
+        x += 0.05
+        align = 'left'
 
-output_filename = os.path.join(
-    base_path,
-    'figures',
-    'airports_map.png'
+    ax.text(x, y, name, transform=proj_lat_lon, zorder=4, ha=align, size=8)
+
+ax.scatter(xs, ys, facecolor=selected_color, s=11, zorder=3)
+ax.scatter(minor_xs, minor_ys, facecolor=major_color, s=5, zorder=3)
+
+# Legend
+legend_handles = [
+    mpatches.Patch(color=selected_color, label='Study Airports'),
+    mpatches.Patch(color=major_color, label='Other Major Airports')
+]
+plt.legend(
+    handles=legend_handles,
+    loc='lower left'
 )
-plt.savefig(output_filename)
+
+save_fig(output_filename)
